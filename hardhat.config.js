78c0758dbdabaa4f1edbcd6cd566f16feeb010d4
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2022-10-14 16:01:30 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("solidity-coverage")
 require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
